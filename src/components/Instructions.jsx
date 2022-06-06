aa2ede1f6555f70372fc2f61b71204f4a12503fd
Ronin Cunningham <ronincunningham1@gmail.com>
2022-06-05 20:02:25 -0700

    minor fix: Changed class to className

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -4,7 +4,7 @@ export const Instructions = (props) => {
 	return (
 		<div className="modal" >
 			<div className="modal-content">
-				<span class="close" onClick={() => props.setShow(false)}>&times;</span>
+				<span className="close" onClick={() => props.setShow(false)}>&times;</span>
 					<div className="container">
 					<h4>Instructions</h4>
 					<p>{props.instructions}</p>
